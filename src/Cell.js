df878e02e598347bddcc092a4a31df551dc26769
github username <github email address>
2019-09-18 18:00:02 -0700

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -9,9 +9,16 @@ export default class Cell extends Component {
     }
   }
   
+  handleCellClick = () => {
+    let newCellColor = this.props.getSelectedColor()
+    this.setState({
+      color: newCellColor
+    })
+  }
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" style={{backgroundColor: this.state.color}}onClick={this.handleCellClick}>
       </div>
     )
   }
